efd609597127362c9b504a37cf479c5fa7c6f543
xem-fullsail <kmoniz@student.fullsail.edu>
2022-05-12 13:08:19 -0400

    Refactor MainWindow. All buttons are now stored in an unordered map, and have bound their events without using an event table.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -4,30 +4,6 @@
 #include "MainWindow.h"
 #include "wx/gbsizer.h"
 
-// The collection of events that this window will interact with
-wxBEGIN_EVENT_TABLE(MainWindow, wxFrame)
-	EVT_BUTTON(9000, OnButtonClicked)
-	EVT_BUTTON(9001, OnButtonClicked)
-	EVT_BUTTON(9002, OnButtonClicked)
-	EVT_BUTTON(9003, OnButtonClicked)
-	EVT_BUTTON(9004, OnButtonClicked)
-	EVT_BUTTON(9005, OnButtonClicked)
-	EVT_BUTTON(9006, OnButtonClicked)
-	EVT_BUTTON(9007, OnButtonClicked)
-	EVT_BUTTON(9008, OnButtonClicked)
-	EVT_BUTTON(9009, OnButtonClicked)
-	EVT_BUTTON(9010, OnButtonClicked)
-	EVT_BUTTON(9011, OnButtonClicked)
-	EVT_BUTTON(9012, OnButtonClicked)
-	EVT_BUTTON(9013, OnButtonClicked)
-	EVT_BUTTON(9014, OnButtonClicked)
-	EVT_BUTTON(9015, OnButtonClicked)
-	EVT_BUTTON(9016, OnButtonClicked)
-	EVT_BUTTON(9017, OnButtonClicked)
-	EVT_BUTTON(9018, OnButtonClicked)
-	EVT_BUTTON(9019, OnButtonClicked)
-	EVT_BUTTON(9020, OnButtonClicked)
-wxEND_EVENT_TABLE()
 
 // Constructor
 MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "calculator.xem", wxPoint(400, 400)) {
@@ -35,50 +11,50 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "calculator.xem", wxPoint(
 	wxGridBagSizer* grid = new wxGridBagSizer();
 
 	// Create the buttons
-	button0 = new wxButton(this, 9000, "0");
-	button1 = new wxButton(this, 9001, "1");
-	button2 = new wxButton(this, 9002, "2");
-	button3 = new wxButton(this, 9003, "3");
-	button4 = new wxButton(this, 9004, "4");
-	button5 = new wxButton(this, 9005, "5");
-	button6 = new wxButton(this, 9006, "6");
-	button7 = new wxButton(this, 9007, "7");
-	button8 = new wxButton(this, 9008, "8");
-	button9 = new wxButton(this, 9009, "9");
-	buttonEql = new wxButton(this, 9010, "=");
-	buttonAdd = new wxButton(this, 9011, "+");
-	buttonSub = new wxButton(this, 9012, "-");
-	buttonMul = new wxButton(this, 9013, "*");
-	buttonDiv = new wxButton(this, 9014, "/");
-	buttonMod = new wxButton(this, 9015, "%");
-	buttonNeg = new wxButton(this, 9016, "+/-");
-	buttonClr = new wxButton(this, 9017, "C");
-	buttonDec = new wxButton(this, 9018, "DEC");
-	buttonBin = new wxButton(this, 9019, "BIN");
-	buttonHex = new wxButton(this, 9020, "HEX");
+	CreateButton('0');
+	CreateButton('1');
+	CreateButton('2');
+	CreateButton('3');
+	CreateButton('4');
+	CreateButton('5');
+	CreateButton('6');
+	CreateButton('7');
+	CreateButton('8');
+	CreateButton('9');
+	CreateButton('=');
+	CreateButton('+');
+	CreateButton('-');
+	CreateButton('*');
+	CreateButton('/');
+	CreateButton('%');
+	CreateButton('N', "+/-");
+	CreateButton('C', "Clr");
+	CreateButton('D', "Dec");
+	CreateButton('B', "Bin");
+	CreateButton('H', "Hex");
 
 	// Add the buttons to the grid, ensuring they expand to fill empty space when the window is resized
-	grid->Add(button0, wxGBPosition(7, 0), wxGBSpan(1, 2), wxALL|wxEXPAND);
-	grid->Add(button1, wxGBPosition(6, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button2, wxGBPosition(6, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button3, wxGBPosition(6, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button4, wxGBPosition(5, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button5, wxGBPosition(5, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button6, wxGBPosition(5, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button7, wxGBPosition(4, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button8, wxGBPosition(4, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(button9, wxGBPosition(4, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonEql, wxGBPosition(6, 3), wxGBSpan(2, 1), wxALL|wxEXPAND);
-	grid->Add(buttonAdd, wxGBPosition(5, 3), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonSub, wxGBPosition(4, 3), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonMul, wxGBPosition(3, 3), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonDiv, wxGBPosition(3, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonMod, wxGBPosition(3, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonNeg, wxGBPosition(7, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonClr, wxGBPosition(3, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonDec, wxGBPosition(0, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonBin, wxGBPosition(1, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
-	grid->Add(buttonHex, wxGBPosition(2, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['0'], wxGBPosition(7, 0), wxGBSpan(1, 2), wxALL | wxEXPAND);
+	grid->Add(buttons['1'], wxGBPosition(6, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['2'], wxGBPosition(6, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['3'], wxGBPosition(6, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['4'], wxGBPosition(5, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['5'], wxGBPosition(5, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['6'], wxGBPosition(5, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['7'], wxGBPosition(4, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['8'], wxGBPosition(4, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['9'], wxGBPosition(4, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['='], wxGBPosition(6, 3), wxGBSpan(2, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['+'], wxGBPosition(5, 3), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['-'], wxGBPosition(4, 3), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['*'], wxGBPosition(3, 3), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['/'], wxGBPosition(3, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['%'], wxGBPosition(3, 1), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['N'], wxGBPosition(7, 2), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['C'], wxGBPosition(3, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['D'], wxGBPosition(0, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['B'], wxGBPosition(1, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
+	grid->Add(buttons['H'], wxGBPosition(2, 0), wxGBSpan(1, 1), wxALL|wxEXPAND);
 
 	// Create the text controls, ensuring they can not be edited by the user
 	equationCtrl = new wxTextCtrl(this, 9100, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
@@ -105,8 +81,6 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "calculator.xem", wxPoint(
 	// Display the grid
 	this->SetSizerAndFit(grid);
 	grid->Layout();
-	
-	//wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
 }
 
 // Destructor
@@ -114,24 +88,32 @@ MainWindow::~MainWindow() {
 	//
 }
 
+// Creates a button, inserts it into the dictionary, and returns it.
+wxButton* MainWindow::CreateButton(char index, std::string label) {	
+	std::string _label = label;										// Use the given label, unless it is empty
+	if (_label.empty()) _label = index;								// If label isn't given, use the char index	
+	wxButton* button = new wxButton(this, 9000 + index, _label);	// Create the button
+	buttons.insert(bpair(index, button));							// Insert the button into the unordered map
+	button->Bind(wxEVT_BUTTON, &MainWindow::OnButtonClicked, this);	// Bind the button to the click method
+	return button;													// Return the button
+}
+
 // Executes logic when a button is clicked.
 void MainWindow::OnButtonClicked(wxCommandEvent& eventName) {
+	// Fetch the clicked button from the given event
 	wxButton* button = (wxButton*)eventName.GetEventObject();
 
+	// Append the face value of the button to the end of the equation
 	std::string buttonLabel = (button->GetLabel()).ToStdString();
-	Input(buttonLabel);
+	Append(buttonLabel);
 	equationCtrl->SetLabel(equation);
 
+	// The event has been handled: end it
 	eventName.Skip();
 }
 
 // Appends the given data to the end of the equation.
-std::string MainWindow::Input(std::string data) {
+std::string MainWindow::Append(std::string data) {
 	equation += data;
 	return equation;
-}
-
-// Calculates the result of the current equation.
-int MainWindow::CalculateResult() {
-	return 0;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -3,47 +3,29 @@
 
 #pragma once
 #include <string>
+#include <unordered_map>
 #include "wx/wx.h"
+
+// Button Pair: std::pair<char, wxButton*>
+using bpair = std::pair<char, wxButton*>;
 
 
 class MainWindow : public wxFrame
 {
 
 public:
-	MainWindow();										// Constructor
-	~MainWindow();										// Destructor
+	MainWindow();												// Constructor
+	~MainWindow();												// Destructor
 
 private:
-	std::string equation = "";							// The current equation being input
-	std::string result = "0";							// The current result of the equation
-	wxTextCtrl* equationCtrl = nullptr;					// The text field to display the equation
-	wxTextCtrl* resultCtrl = nullptr;					// The text field to display the result
-	wxButton* button0 = nullptr;						// Button '0'
-	wxButton* button1 = nullptr;						// Button '1'
-	wxButton* button2 = nullptr;						// Button '2'
-	wxButton* button3 = nullptr;						// Button '3'
-	wxButton* button4 = nullptr;						// Button '4'
-	wxButton* button5 = nullptr;						// Button '5'
-	wxButton* button6 = nullptr;						// Button '6'
-	wxButton* button7 = nullptr;						// Button '7'
-	wxButton* button8 = nullptr;						// Button '8'
-	wxButton* button9 = nullptr;						// Button '9'
-	wxButton* buttonEql = nullptr;						// Button '='
-	wxButton* buttonAdd = nullptr;						// Button '+'
-	wxButton* buttonSub = nullptr;						// Button '-'
-	wxButton* buttonMul = nullptr;						// Button '*'
-	wxButton* buttonDiv = nullptr;						// Button '/'
-	wxButton* buttonMod = nullptr;						// Button '%'
-	wxButton* buttonNeg = nullptr;						// Button '+/-'
-	wxButton* buttonClr = nullptr;						// Button 'Clear'
-	wxButton* buttonDec = nullptr;						// Button 'Decimal'
-	wxButton* buttonBin = nullptr;						// Button 'Binary'
-	wxButton* buttonHex = nullptr;						// Button 'Hexadecimal'
-
-	void OnButtonClicked(wxCommandEvent& eventName);	// Executes logic when a button is clicked.
-	std::string Input(std::string data);				// Appends the given data to the end of the equation.
-	int CalculateResult();								// Calculates the result of the current equation.
-
-	wxDECLARE_EVENT_TABLE();							// Creates an event table for this window.
+	std::string equation = "";									// The current equation being input
+	std::string result = "0";									// The current result of the equation
+	wxTextCtrl* equationCtrl = nullptr;							// The text field to display the equation
+	wxTextCtrl* resultCtrl = nullptr;							// The text field to display the result
+	std::unordered_map<char, wxButton*> buttons;				// The collection of buttons indexed by their label
+	
+	wxButton* CreateButton(char index, std::string label = "");	// Creates and returns a button.
+	std::string Append(std::string data);						// Appends the given data to the end of the equation.
+	void OnButtonClicked(wxCommandEvent& eventName);			// Executes logic when a button is clicked.
 
 };
